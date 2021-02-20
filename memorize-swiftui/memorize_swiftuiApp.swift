1796f2a5a2c29d72548c0f17f6f89ff96d9b67e4
Josip Rezić <josip@feelmorelabs.com>
2021-02-20 14:40:35 +0100

    Add files

---
+++ memorize-swiftui/memorize_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorize_swiftuiApp.swift
+//  memorize-swiftui
+//
+//  Created by Josip Rezić on 2/20/21.
+//
+
+import SwiftUI
+
+@main
+struct memorize_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
